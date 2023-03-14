a7b7e9ece641ba23dc120e0afddd25d6d084174e
Scott Jones <itspinky95@hotmail.com>
2023-03-14 17:03:53 +0000

    Deplaying to firebase

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,9 @@ import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { getStorage } from 'firebase/storage';
 import { getFirestore } from 'firebase/firestore/lite';
 import { getAnalytics } from "firebase/analytics";
-import {firebaseConfig} from '../config/firebase.js/'
+import { firebaseConfig } from '../config/firebase.js/'
+import { getPerformance } from "firebase/performance";
+import { getFunctions } from "firebase/functions";
 
 
 // Initialize Firebase
@@ -13,6 +15,8 @@ const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
 export const storage = getStorage(app);
 export const analytics = getAnalytics(app);
+export const performance = getPerformance(app);
+export const functions = getFunctions(app);
 
 
 export const signInWithGoogle = () => signInWithPopup(auth, provider);
