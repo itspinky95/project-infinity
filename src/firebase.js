eb22c4ff464f251758050e760f3560f4fb711322
itspinky95 <itspinky95@hotmail.com>
2023-03-16 11:39:25 +0000

    Bug fixing with firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,29 +1,23 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
-import { getFirestore } from 'firebase/firestore/lite';
-
-// Your web app's Firebase configuration
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAYPH1lwPEo46LDloQ6v2vyepAmvIOvfjw",
-  authDomain: "project-infinity-1995.firebaseapp.com",
-  databaseURL: "https://project-infinity-1995-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "project-infinity-1995",
-  storageBucket: "project-infinity-1995.appspot.com",
-  messagingSenderId: "188436326894",
-  appId: "1:188436326894:web:ce9c5d7dd6aea8326406ff",
-  measurementId: "G-WZR5MDEXCB"
-};
-
+import { getAnalytics } from 'firebase/analytics';
+import { getPerformance } from 'firebase/performance';
+import { getFunctions } from 'firebase/functions';
+import firebaseConfig from './config/firebase.js';
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth();
+
+// Initialize Firebase services
+export const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+export const analytics = getAnalytics(app);
+export const performance = getPerformance(app);
+export const functions = getFunctions(app);
 
-
+// Export Google sign in method
 export const signInWithGoogle = () => signInWithPopup(auth, provider);
